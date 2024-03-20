59cf1ec6145306b1de31539e9bfa3a88496c727f
Joe <53631725+JustJoeYo@users.noreply.github.com>
2024-03-20 15:15:40 -0700

    push 5

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.min.css'
 import {
   Dashboard,
   Projects,
+  PageNotFound,
   Login,
   Signup,
   PrivateRoutes,
@@ -17,25 +18,34 @@ import {
   ApiProvider,
   AppContextProviders,
 } from '../contexts/types'
+import BGParticle from './particles/particles'
 
 function App() {
   const providers = [ToastProvider, AuthProvider, ApiProvider]
   return (
-    <Router>
-      <AppContextProviders components={providers}>
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route element={<Dashboard />} path="/" />
-            <Route path="/update-profile" element={<UpdateProfile />} />
-            <Route path="projects" element={<Projects />} />
-          </Route>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-        </Routes>
-      </AppContextProviders>
-    </Router>
+    <>
+      <Router>
+        <AppContextProviders components={providers}>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+              <Route element={<Dashboard />} path="/" />
+              <Route path="/update-profile" element={<UpdateProfile />} />
+              <Route path="projects" element={<Projects />} />
+            </Route>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </AppContextProviders>
+      </Router>
+    </>
   )
 }
 
 export default App
+function createBrowserRouter(
+  arg0: { path: string; children: { path: string; element: JSX.Element }[] }[]
+) {
+  throw new Error('Function not implemented.')
+}
--- src/Components/types.ts
+++ src/Components/types.ts
@@ -6,10 +6,12 @@ import Signup from './authentication/Signup'
 import PrivateRoutes from './authentication/PrivateRoutes'
 import ForgotPassword from './authentication/ForgotPassword'
 import UpdateProfile from './authentication/UpdateProfile'
+import PageNotFound from './pages/pagenotfound'
 
 export {
   Dashboard,
   Projects,
+  PageNotFound,
   Login,
   Signup,
   PrivateRoutes,
