7f252c3e8c0a4f391a0f03063e0d50d0eaac2ef8
Joe <53631725+JustJoeYo@users.noreply.github.com>
2024-03-30 21:39:43 -0700

    prod push

--- src/Components/pages/Projects.tsx
+++ src/Components/pages/Projects.tsx
@@ -1,8 +1,56 @@
+import { useState } from 'react'
+import { useToast, EToastTypes } from '../../contexts/types'
+
+let numbaaaa: Number
+
 export default function Projects() {
+  const [loading, setLoading] = useState(false)
+  const { showTypedToast } = useToast()
+
+  async function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      if (numbaaaa != undefined && numbaaaa != null) {
+        showTypedToast(
+          EToastTypes.SUCCESS,
+          'The number you were thinking of was ' + numbaaaa + '!'
+        )
+      } else {
+        showTypedToast(
+          EToastTypes.ERROR,
+          'WRONG FORMAT! Please use your eyes before submitting again!'
+        )
+      }
+    } catch {
+      showTypedToast(EToastTypes.ERROR, 'FAIL')
+    }
+
+    setLoading(false)
+  }
+
   return (
     <>
-      <div className="bg-popclr text-textclr outline outline-outlineclr h-full m-5 p-5">
-        Projects
+      <div className="bg-popclr text-textclr outline outline-outlineclr h-full p-5 m-5">
+        <div className="align-middle justify-center text-center flex">
+          <input
+            id="numbaaaa"
+            placeholder="Number You Are Thinking Of"
+            className="text-lg outline outline-outlineclr h-24 w-64 my-20"
+            onInput={(e) => (numbaaaa = Number(e.currentTarget.value))}
+            type="text"
+            pattern="[0-9]*"
+          ></input>
+          <button
+            type="submit"
+            disabled={loading}
+            onClick={handleSubmit}
+            className="text-2xl outline outline-outlineclr h-24 w-64 my-20"
+          >
+            GUESS
+          </button>
+        </div>
       </div>
     </>
   )
